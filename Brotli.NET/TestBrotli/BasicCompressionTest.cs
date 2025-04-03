bbe85705541c9218501368b32aef8b469bb99e83
Richard Fox <richard.fox@ahref.co.uk>
2025-04-03 01:21:01 -0700

    tweak(tests): move FX45 Tests to Main test project and rename
    These tests were failing in the FX45 Project as it was not setup to have the native libraries, it also looks to be ancient. There's no real documentaiton that I can find about why its a separate project.
    
    Its commit is here: https://github.com/XieJJ99/brotli.net/commit/0f4bc44c8466cc19777612639ca225de7cbcd644
    
    But it works in the regular test project.

---
+++ Brotli.NET/TestBrotli/BasicCompressionTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Brotli;
+using Xunit;
+
+namespace TestBrotli
+{
+    public class BasicCompressionTest
+    {
+        [Fact]
+        public void TestCreateInstance()
+        {
+            var input = new byte[] { 1, 2, 3, 4, 1, 2, 3, 4 };
+            Byte[] output = input.CompressToBrotli();
+            Assert.Equal(new byte[] { 0x8b, 0x03, 0x80, 1, 2, 3, 4, 1, 2, 3, 4, 0x3 }, output);
+        }
+    }
+}
