dac1a733d0bb416108d4874864f8b7d685f940ac
Richard Fox <richard.fox@ahref.co.uk>
2025-04-03 01:07:53 -0700

    feat(tests): add dead simple easy to validate native library test

---
+++ Brotli.NET/TestBrotli/NativeLibraryLoading.cs
@@ -0,0 +1,17 @@
+﻿using Brotli;
+using System;
+using Xunit;
+
+namespace TestBrotli
+{
+    public class NativeLibraryLoading
+    {
+        [Fact]
+        public void TestLibraryLoads()
+        {
+            IntPtr res = Brolib.BrotliEncoderCreateInstance();
+
+            Assert.NotEqual(IntPtr.Zero, res); // A valid pointer must be returned
+        }
+    }
+}
