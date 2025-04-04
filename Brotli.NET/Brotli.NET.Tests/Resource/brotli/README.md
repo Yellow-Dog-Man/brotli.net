c624f9954f6792b684d55e87b119903ef73003f1
Richard Fox <richard.fox@ahref.co.uk>
2025-04-03 22:53:10 -0700

    feat(tests): add files from brotli main repo for diagnostic reasons

---
+++ Brotli.NET/Brotli.NET.Tests/Resource/brotli/README.md
@@ -0,0 +1,7 @@
+# Fixtures
+
+Test Fixures/Files from: https://github.com/Yellow-Dog-Man/brotli/tree/master/tests/testdata
+
+Anything "weird", was skipped. We were purely looking for uncompressed -> compressed.
+
+Also we have no idea what quality or window settings were used >.<.
---
+++ Brotli.NET/Brotli.NET.Tests/Snapshot/BrotliSnapshotting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brotli.NET.Tests.Snapshot
+{
+    public class BrotliSnapshotting
+    {
+        private static string ROOT_PATH => CurrentFile.Directory();
+        private static string SNAPSHOT_PATH => Path.Combine(ROOT_PATH, "..", "Resource/brotli");
+
+        [Fact]
+        public void ValidateBrotli()
+        {
+            var files = Directory.EnumerateFiles(SNAPSHOT_PATH);
+            var uncompressed = files.Where(file => !IsCompressed(file));
+            var compressed = files.Where(file => IsCompressed(file));
+
+            foreach (string uncompressFilePath in uncompressed)
+            {
+                Console.WriteLine(uncompressFilePath);
+                Console.WriteLine(Path.GetExtension(uncompressFilePath));
+                var compressedFilePath = compressed.FirstOrDefault(file => file.StartsWith(uncompressFilePath));
+                if (string.IsNullOrEmpty(compressedFilePath))
+                {
+                    Console.WriteLine("NO Match");
+                    continue;
+                }
+                    
+
+                var inputBytes = File.ReadAllBytes(uncompressFilePath);
+                var expectedBytes = File.ReadAllBytes(compressedFilePath);
+
+                var outputBytes = inputBytes.CompressToBrotli();
+
+                Assert.Equal(expectedBytes, outputBytes);
+            }
+        }
+
+        private bool IsCompressed(string file)
+        {
+            return file.EndsWith("compressed");
+        }
+    }
+}
